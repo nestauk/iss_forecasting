2a96b788a98eccce7b7e8b93c93fc145c9f70491
Jack Rasala <jack.rasala@nesta.org.uk>
2022-01-18 12:09:26 +0000

    Add lag plot functions

--- iss_forecasting/analysis/utils/plotting.py
+++ iss_forecasting/analysis/utils/plotting.py
@@ -1,6 +1,28 @@
 """Analysis utils for plotting
 """
 import altair as alt
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
+import seaborn as sns
+import math
+
+# Set Matplotlib defaults
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True, figsize=(11, 4))
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=16,
+    titlepad=10,
+)
+plot_params = dict(
+    color="0.75",
+    style=".-",
+    markeredgecolor="0.25",
+    markerfacecolor="0.25",
+)
 
 
 def plot_two_y_one_x(
@@ -40,3 +62,91 @@ def plot_two_y_one_x(
         alt.Y(y2, axis=alt.Axis(titleColor=line_2_colour))
     )
     return alt.layer(line_1, line_2).resolve_scale(y="independent")
+
+
+def lagplot(x, y=None, lag=1, ax=None, **kwargs):
+    """Plots a single lag plot for time series data.
+    Can plot lagged x vs x or lagged x vs y.
+
+    Args:
+        x (pd.Series): x time series to be lagged
+        y (pd.Series, optional): y time series, defaults to None. If None,
+            y will be set to x
+        lag (int, optional): how much to lag x time series by, defaults to 1.
+        ax (int, optional): axes to draw the plot onto, defaults to None.
+
+    Returns:
+        matplotlib.axes._subplots.AxesSubplot: Single lag plot
+
+    This function is adapted from https://www.kaggle.com/ryanholbrook/time-series-as-features
+    """
+    x_ = x.shift(lag)
+    y_ = y if y is not None else x
+    corr = y_.corr(x_)
+    if ax is None:
+        _, ax = plt.subplots()
+    scatter_kws = dict(
+        alpha=0.75,
+        s=3,
+    )
+    line_kws = dict(
+        color="C3",
+    )
+    ax = sns.regplot(
+        x=x_,
+        y=y_,
+        scatter_kws=scatter_kws,
+        line_kws=line_kws,
+        lowess=False,
+        order=1,
+        ci=None,
+        ax=ax,
+        **kwargs,
+    )
+    at = AnchoredText(
+        f"{corr:.2f}",
+        prop=dict(size="medium"),
+        frameon=True,
+        loc="upper right",
+    )
+    ax.add_artist(at)
+    ax.xaxis.label.set_size(8.5)
+    ax.yaxis.label.set_size(8.5)
+    return ax
+
+
+def plot_lags(x, title, y=None, lags=6, nrows=1, lagplot_kwargs={}, **kwargs):
+    """Plots multple lag plots for time series data.
+    Can plot lagged x vs x or lagged x vs y.
+
+    Args:
+        x (pd.Series): x time series to be lagged
+        title (str): title information to display in addition to
+            number of lags
+        y (pd.Series, optional): y time series, defaults to None.
+            If None, y will be set to x
+        lags (int, optional): how many lags to display, defaults to 6.
+        nrows (int, optional): number of rows to split the plots over,
+            defaults to 1.
+        lagplot_kwargs (dict, optional): defaults to {}.
+
+    Returns:
+        matplotlib.figure.Figure: Multiple lag plots
+
+    This function is adapted from https://www.kaggle.com/ryanholbrook/time-series-as-features
+    """
+    kwargs.setdefault("nrows", nrows)
+    kwargs.setdefault("ncols", math.ceil(lags + 1 / nrows))
+    kwargs.setdefault("figsize", (kwargs["ncols"] * 2, nrows * 2 + 0.5))
+    fig, axs = plt.subplots(sharex=True, sharey=True, squeeze=False, **kwargs)
+    for ax, k in zip(fig.get_axes(), range(kwargs["nrows"] * kwargs["ncols"])):
+        if k <= lags + 1:
+            ax = lagplot(x, y, lag=k, ax=ax, **lagplot_kwargs)
+            ax.set_title(f"Lag {k} {title}", fontdict=dict(fontsize=8.5))
+            ax.set(xlabel="", ylabel="")
+        else:
+            ax.axis("off")
+    plt.setp(axs[-1, :], xlabel=x.name)
+    plt.setp(axs[:, 0], ylabel=y.name if y is not None else x.name)
+    fig.tight_layout(w_pad=0.1, h_pad=0.1)
+    return fig
