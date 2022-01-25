6b0d55d54a1df52050b7dc837dac4dd8f4508b8d
Jack Rasala <jack.rasala@nesta.org.uk>
2022-01-25 09:58:58 +0000

    Update stationarity test docstrings to have type hints

--- iss_forecasting/utils/stats_tests.py
+++ iss_forecasting/utils/stats_tests.py
@@ -1,27 +1,40 @@
 """Statistical testing utils
 """
 from statsmodels.tsa.stattools import adfuller, kpss
+from numpy.typing import ArrayLike
 
 
-def stationarity_adf(time_series, name):
-    "Check if the time series is stationary based on ADF test"
+def stationarity_adf(time_series: ArrayLike, name: str) -> None:
+    """Check if a time series is stationary based on ADF test
+    and print the results
+
+    Args:
+        time_series: Time series to be checked
+        name: Name of time series
+    """
     results = adfuller(time_series)
     p_value = round(results[1], 3)
     n_lags = results[2]
     print(
         f"{name} ADF test: p_value is {p_value}. {p_value}"
-        f'{" < 0.05, time series is" if p_value < 0.05 else " > 0.05, time series is not"}'
+        f"{' < 0.05, time series is' if p_value < 0.05 else ' > 0.05, time series is not'}"
         f" stationary. {n_lags} lags"
     )
 
 
-def stationarity_kpss(time_series, name):
-    "Check if the time series is stationary based on KPSS test"
+def stationarity_kpss(time_series: ArrayLike, name: str) -> None:
+    """Check if a time series is stationary based on KPSS test
+    and print the results
+
+    Args:
+        time_series: Time series to be checked
+        name: Name of time series
+    """
     results = kpss(time_series)
     p_value = round(results[1], 3)
     n_lags = results[2]
     print(
         f"{name} KPSS test: p_value is {p_value}. {p_value}"
-        f'{" < 0.05, time series is not" if p_value < 0.05 else " > 0.05, time series is"}'
+        f"{' < 0.05, time series is not' if p_value < 0.05 else ' > 0.05, time series is'}"
         f" stationary. {n_lags} lags"
     )
