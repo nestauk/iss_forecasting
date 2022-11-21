38f91a5f66794f69c5323c980de3537e8fdef852
Jack Rasala <jack@jrds.io>
2022-11-21 19:03:05 +0900

    Update getter to have split option

--- iss_forecasting/getters/company_success.py
+++ iss_forecasting/getters/company_success.py
@@ -4,7 +4,10 @@ from iss_forecasting import PROJECT_DIR
 
 
 def get_company_future_success_dataset(
-    date_range: str = "2011-01-01-2019-01-01", uk_only: bool = True, test: bool = False
+    date_range: str = "2011-01-01-2019-01-01",
+    uk_only: bool = True,
+    test: bool = False,
+    split: int = None,
 ) -> pd.DataFrame:
     """Load company level future success dataset
 
@@ -12,6 +15,7 @@ def get_company_future_success_dataset(
         date_range: Date range in the format - 2011-01-01-2019-01-01
         uk_only: True to load a uk only data, False to load a worldwide data
         test: True to load a test dataset, False to load a full dataset
+        split: Int relating to the split value of the file to be loaded
 
     Returns:
         Dataset of companies including information about investments,
@@ -19,9 +23,10 @@ def get_company_future_success_dataset(
     """
     test_indicator = "_test" if test else ""
     region_indicator = "_ukonly" if uk_only else "_worldwide"
+    split_indicator = f"_split_{split}" if split else ""
     return pd.read_csv(
         PROJECT_DIR
-        / f"inputs/data/company_level/company_data_window_{date_range}{test_indicator}{region_indicator}.csv",
+        / f"inputs/data/company_level/company_data_window_{date_range}{test_indicator}{region_indicator}{split_indicator}.csv",
         index_col=0,
         dtype={
             "location_id": "category",
